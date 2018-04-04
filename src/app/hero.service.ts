ad0bb96c22f348461d91883130917f57bc1d696c
tshevchenko <tshevchenko@ro.ru>
2018-04-04 17:33:13 +0300

    'Stem-6'

---
+++ src/app/hero.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+import { Observable } from 'rxjs/Observable';
+import { of } from 'rxjs/observable/of';
+
+import { MessageService } from './message.service';
+
+@Injectable()
+export class HeroService {
+
+  getHeroes(): Observable<Hero[]> {
+    this.messageService.add('HeroService: fetched heroes');
+    this.messageService.add('Еще одно сообщение');
+    return of(HEROES);
+  }
+
+  constructor( private messageService: MessageService) { }
+
+}
