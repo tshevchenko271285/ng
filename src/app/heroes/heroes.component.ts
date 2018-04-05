e5d956af1ad1f750681df92b441f6c277eaae6d3
tshevchenko <tshevchenko@ro.ru>
2018-04-05 15:45:59 +0300

    'Step-7'

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -10,18 +10,14 @@ import { HeroService } from "../hero.service";
 export class HeroesComponent implements OnInit {
 
   heroes: Hero[];
-  selectedHero: Hero;
 
-  onSelect(hero: Hero): void{
-    this.selectedHero = hero;
-  }
-  getHeroes(): void {
-    this.heroService.getHeroes().subscribe( heroes => this.heroes = heroes );
-  }
   constructor( private heroService: HeroService ) { }
 
   ngOnInit() {
     this.getHeroes();
   }
 
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe( heroes => this.heroes = heroes );
+  }
 }
